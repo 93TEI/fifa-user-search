c287333228e28ed8a7947152eff967d66e8ba3e6
93TEI <43162525+93TEI@users.noreply.github.com>
2021-12-15 01:02:42 +0900

    Update UserApiController.java

--- src/main/java/com/search/fifa/web/UserApiController.java
+++ src/main/java/com/search/fifa/web/UserApiController.java
@@ -4,7 +4,6 @@ import com.search.fifa.service.UserService;
 import com.search.fifa.web.dto.UserInfoResponseDto;
 import com.search.fifa.web.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
